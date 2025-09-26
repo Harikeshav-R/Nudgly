326f765e514cd6ea17bafff9add5ad94c1b7492d
Harikeshav R <r.harikeshav@gmail.com>
2025-09-25 22:50:20 -0400

    **feat(app): set application and organization details in main entry point**
    - Added application name, organization name, and organization domain setup in `main.cpp` for proper metadata configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char* argv[])
 {
     QGuiApplication application(argc, argv);
+    QGuiApplication::setApplicationName("Nudgly");
+    QGuiApplication::setOrganizationName("Nudgly");
+    QCoreApplication::setOrganizationDomain("Nudgly.com");
+
     QQmlApplicationEngine engine;
 
     engine.load(QUrl("qrc:/views/main.qml"));
